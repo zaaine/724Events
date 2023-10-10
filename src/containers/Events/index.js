f9e7d7307c56ae603d290b3bf435ea6c2da04ea3
Antoniutti Pierre <pierre.antoniutti@gmail.com>
2023-10-10 16:08:13 +0200

    Reparation du probleme de "path" incomprehensible en recommencant le projet

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+// Events/index.js
+import { useState} from "react";
 import EventCard from "../../components/EventCard";
 import Select from "../../components/Select";
 import { useData } from "../../contexts/DataContext";
@@ -9,29 +10,44 @@ import "./style.css";
 
 const PER_PAGE = 9;
 
+// Quand on slectionne un element du tableau, il faut que la section EventList se rafraichisse
+// En prenant en compte le type selectionné pour afficher les bons events
+// Il faut comparer le type selectionné et le type des events pour les mettre dans un tableau
+// Pour le moment le useState type est null meme quand on change de selection
+
 const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
+  // eslint-disable-next-line no-unused-vars
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+    (!type ? data?.events : data?.events.filter((event)=> event.type === type) ))
+
+  // Faire un nouveau tableau depuis le filteredEvents if type === element.type ? 
+  // Probleme : cela ne prend en compte que les element affiché dans la page courante
+
+
+  // Faire un nouveau tableau depuis data.events if type === element.type ?
+  // Quand type change , actualiser la liste afficher pour la comparer avec type
+  // Si type est null alors toute la filteredEvents est affiché
+  // Si type est non null alors faire la liste des elements === a type et les afficher
+  // Comment faire pour que la liste se rafraichisse quand on change de type ?
+
+  // Si !type ? filteredEvents.map((event) => <Modal key={event.id} Content={<ModalEvent event={event} />}>{({ setIsOpened }) => (<EventCard onClick={() => setIsOpened(true)} imageSrc={event.cover} title={event.title} date={new Date(event.date)} label={event.type}/>)}</Modal>)) : filteredType.map((event) => <Modal key={event.id} Content={<ModalEvent event={event} />}>{({ setIsOpened }) => (<EventCard onClick={() => setIsOpened(true)} imageSrc={event.cover} title={event.title} date={new Date(event.date)} label={event.type}/>)}</Modal>))
+
+
+
+  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+  // Le tableau de filtres
+  const typeList = new Set(data?.events.map((event) => event.type));
+
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
-  };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  const typeList = new Set(data?.events.map((event) => event.type));
+  }
+
+
   return (
     <>
       {error && <div>An error occured</div>}
@@ -41,8 +57,15 @@ const EventList = () => {
         <>
           <h3 className="SelectTitle">Catégories</h3>
           <Select
+            id="inputSelection"
+            // selection n'existe pas dans le select 
             selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
+            // Si value est null alors on affiche tout les elements mais si il est comme l'une des valeurs
+            // du tableau alors on affiche que les elements qui ont le type selectionné
+            onChange={(value) => 
+              (value ? changeType(value) : changeType(null))
+            
+            }
           />
           <div id="events" className="ListContainer">
             {filteredEvents.map((event) => (
--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,23 +8,26 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
+  new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+);
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index + 1 < byDateDesc.length ? index + 1 : 0),
       5000
     );
   };
   useEffect(() => {
+    if(data){
     nextCard();
-  });
+    }
+  },[index]);
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+        <div
+          key={Math.random()*100}
+        >
           <div
-            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
@@ -42,15 +45,16 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={Math.random()*100}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
+                  readOnly 
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );

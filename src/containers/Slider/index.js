6958c5d3dca5d913757a0182304bd6bfd13df0af
Bruno Vang <buens@outlook.fr>
2024-01-04 15:20:32 +0100

    Slider image blanche

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,7 +12,7 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < (byDateDesc?.length || 0) - 1 ? index + 1 : 0),
       5000
     );
   };

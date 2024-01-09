e6ec69745fb7af5d5d7812b09d74d5b71eb19510
Bruno Vang <buens@outlook.fr>
2024-01-09 15:37:37 +0100

    FailedTest add advanceTimer Form to Modal

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
 import Form from "./index";
 
 // Activation des faux timer avant le test (simulation de temps)
@@ -26,10 +25,8 @@ describe("When Events is created", () => {
         })
       );
 
-      // Avance le temps d'attente du timer provenant de mockContactApi
-      act(() => {
-        jest.advanceTimersByTime(1000);
-      });
+      // Avance le temps d'attente du timer
+      jest.advanceTimersByTime(1000);
 
       await screen.findByText("En cours");
       await screen.findByText("Envoyer");

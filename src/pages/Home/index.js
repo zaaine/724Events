af6547c788fbfeac4b8483d46a62491849b4a15e
aziz zaaine <zaaine@live.fr>
2024-06-19 12:56:10 +0200

    Scénario 4 : Quand je click sur un onglet du menu, cela me dirige vers la section concernée. Rajout des ancres et des ID pour navigation du menu vers les sections concernées.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -23,7 +23,7 @@ const Page = () => {
         <Slider />
       </section>
       <section className="ServicesContainer">
-        <h2 className="Title">Nos services</h2>
+        <h2 id="nos-services" className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
           <ServiceCard imageSrc="/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png">
@@ -51,11 +51,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section id="nos-realisations" className="EventsContainer">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section id="notre-equipe" className="PeoplesContainer">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">

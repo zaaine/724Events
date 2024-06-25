3b0ac2652e36a5fae5871ad486f72d7387c6e688
aziz zaaine <zaaine@live.fr>
2024-06-25 19:16:16 +0200

    Création d'un test Perso qui vient vérifier le nettoyage du formulaire aprés soumissions

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -27,3 +27,44 @@ describe("When Events is created", () => {
     });
   });
 });
+
+
+
+// Mon test : 
+
+  describe("and a click is triggered on the submit button", () => {
+    it("the input form is empty after submission", async () => {
+      const onSuccess = jest.fn();
+      render(<Form onSuccess={onSuccess} />);
+
+      const nomInput = screen.getByLabelText("Nom");
+      const prenomInput = screen.getByLabelText("Prénom");
+      const emailInput = screen.getByLabelText("Email");
+      const messageInput = screen.getByLabelText("Message");
+
+      console.log('nomInput:', nomInput);
+      console.log('prenomInput:', prenomInput);
+      console.log('emailInput:', emailInput);
+      console.log('messageInput:', messageInput);
+
+      fireEvent.change(nomInput, { target: { value: "Doe" } });
+      fireEvent.change(prenomInput, { target: { value: "John" } });
+      fireEvent.change(emailInput, { target: { value: "john.doe@example.com" } });
+      fireEvent.change(messageInput, { target: { value: "Test message" } });
+
+      fireEvent(
+        await screen.findByTestId("button-test-id"),
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+
+      await waitFor(() => expect(onSuccess).toHaveBeenCalled());
+
+      // Vérifier que les champs du formulaire ont été réinitialisés
+      expect(nomInput.value).toBe("");
+      expect(prenomInput.value).toBe("");
+      expect(emailInput.value).toBe("");
+      expect(messageInput.value).toBe("");
+    },)})
